223966c6568b5d908f3d86e349116b10a0077264
merceyz <merceyz@users.noreply.github.com>
2020-11-21 20:29:16 +0100

    chore(cli): throw on broken node 14 releases

--- packages/yarnpkg-cli/sources/main.ts
+++ packages/yarnpkg-cli/sources/main.ts
@@ -60,12 +60,19 @@ export async function main({binaryVersion, pluginConfiguration}: {binaryVersion:
     // Non-exhaustive known requirements:
     // - 10.16+ for Brotli support on `plugin-compat`
     // - 10.17+ to silence `got` warning on `dns.promises`
+    // - 14.0 and 14.1 empty http responses - https://github.com/sindresorhus/got/issues/1496
 
     const version = process.versions.node;
     const range = `>=10.17`;
 
-    if (!semverUtils.satisfiesWithPrereleases(version, range) && process.env.YARN_IGNORE_NODE !== `1`)
-      throw new UsageError(`This tool requires a Node version compatible with ${range} (got ${version}). Upgrade Node, or set \`YARN_IGNORE_NODE=1\` in your environment.`);
+    if (process.env.YARN_IGNORE_NODE !== `1`) {
+      if (!semverUtils.satisfiesWithPrereleases(version, range))
+        throw new UsageError(`This tool requires a Node version compatible with ${range} (got ${version}). Upgrade Node, or set \`YARN_IGNORE_NODE=1\` in your environment.`);
+
+      if (semverUtils.satisfiesWithPrereleases(version, `>=14 <= 14.1`)) {
+        throw new UsageError(`Node 14.0 and 14.1 have bugs in their Stream implementation causing empty http responses. Upgrade Node, or set \`YARN_IGNORE_NODE=1\` in your environment.`);
+      }
+    }
 
     // Since we only care about a few very specific settings (yarn-path and ignore-path) we tolerate extra configuration key.
     // If we didn't, we wouldn't even be able to run `yarn config` (which is recommended in the invalid config error message)
